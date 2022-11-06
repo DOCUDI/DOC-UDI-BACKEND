5d73432af67d10ad46b376be9a9100aa39c07957
PAARTH2608 <jain.paarth2608@gmail.com>
2022-11-06 23:58:21 +0530

    add:cors-header-policies

--- index.js
+++ index.js
@@ -11,22 +11,19 @@ connectDB();
 const router = require("./api/routes");
 
 app.use(express.json());
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 app.use(express.urlencoded({ extended: true }));
-// header("Access-Control-Allow-Origin: *");
-// header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS");
-// header(
-//   "Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization"
-// );
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET,PUT,POST,DELETE,PATCH,OPTIONS");
+header(
+  "Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization"
+);
 
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: "https://docudibackend.herokuapp.com",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 
 app.use("/api", router);
 
